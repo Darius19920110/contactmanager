1cecfe40908125c6bf887f4db744da7b608ecb1a
Darius19920110 <meszidani22@gmail.com>
2020-07-25 20:37:55 +0100

    Updates

---
+++ precache-manifest.c3d4d225c63f73cb844f0f233abf94fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c22222084ae5a88ad76c106b939ce2",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "2e55f824118211da4c64",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "37f7bcb73852eb88c98b",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2e55f824118211da4c64",
+    "url": "/contactmanager/static/js/2.cdf35a88.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.cdf35a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37f7bcb73852eb88c98b",
+    "url": "/contactmanager/static/js/main.c8ee2016.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
